e9df3844838bc2a9181460f40c444006eb028a53
StevenRMeli <steven.rojas@mercadolibre.com.co>
2022-01-19 19:03:52 -0500

    conf loader

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,23 +2,24 @@ import { Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import Header from "../components/Header/Header";
+import { Loader } from "../components/Loader";
 import { routes } from "./routes";
 
 export const Navigation = () => {
   return (
-    <Suspense fallback={<span>Loading...</span>}>
-      <BrowserRouter>
-        <Header />
-        <div className="main-layout">
+    <BrowserRouter>
+      <Header />
+      <div className="main-layout">
+        <Suspense fallback={<Loader />}>
           <Routes>
             {routes.map(({ to, path, Component }) => (
               <Route key={to} path={path} element={<Component />} />
             ))}
             <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
           </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
-    </Suspense>
+        </Suspense>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 };
